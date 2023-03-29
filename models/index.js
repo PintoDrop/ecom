fb5756a8df6af969436e148758e0e5e44734715e
PintoDrop <mikebender22@gmail.com>
2023-03-29 11:49:44 -0700

    working on model relations2

--- models/index.js
+++ models/index.js
@@ -16,16 +16,21 @@ Product.belongsTo(Category, {
 // })
 
 // Products belongToMany Tags (through ProductTag)
-// Product.belongsToMany(ProductTag, {
-//   foreignKey: 'productTag_id',
-//   onDelete: 'CASCADE'
-// })
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
 
 // Tags belongToMany Products (through ProductTag)
-// Tags.belongsToMany(Product, {
-//   foreignKey: 'product_id',
-//   onDelete: 'CASCADE'
-// })
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+  // as: 'product_tag'
+});
 
 
 module.exports = {
